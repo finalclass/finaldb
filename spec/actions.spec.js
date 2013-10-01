f28c5534b8a40d2f7ab8ea0fc5c21fd19faa9a79
Szymon WYgnański <s@finalclass.net>
2013-10-01 15:38:41 +0200

    storeRevisions Collection property

--- spec/actions.spec.js
+++ spec/actions.spec.js
@@ -4,161 +4,161 @@
 'use strict';
 
 var fdb = require('../index.js'),
-    fs = require('fs'),
-    paths = require('../lib/paths.js'),
-    ffs = require('final-fs');
+  fs = require('fs'),
+  paths = require('../lib/paths.js'),
+  ffs = require('final-fs');
 
 describe('actions', function () {
-    var cars, car1, car2, rootDir = __dirname + '/var/cars';
+  var cars, car1, car2, rootDir = __dirname + '/var/cars';
 
-    beforeEach(function () {
+  beforeEach(function () {
 
-        cars = new fdb.Collection({dirName: rootDir});
-        car1 = {
-            mark: 'Fiat',
-            model: '126p'
-        };
-        car2 = {
-            mark: 'Volkswagen',
-            model: 'Golf 1'
-        };
+    cars = new fdb.Collection({dirName: rootDir});
+    car1 = {
+      mark: 'Fiat',
+      model: '126p'
+    };
+    car2 = {
+      mark: 'Volkswagen',
+      model: 'Golf 1'
+    };
+  });
+
+  afterEach(function () {
+    ffs.rmdirRecursiveSync(__dirname + '/var');
+  });
+
+  it('can insert', function () {
+    var done = false;
+
+    cars.insert(car1).flush()
+      .then(function () {
+        done = true;
+      }).otherwise(function (err) {
+        console.log(err.stack);
+      });
+
+    waitsFor(function () {
+      return done;
+    }, 'insert', 100);
+
+    runs(function () {
+      expect(
+        fs.existsSync(paths.documentPath(rootDir, car1.id))
+      ).toBe(true);
+    });
+  });
+
+  it('can update', function () {
+    var done = false,
+      oldRev;
+
+    cars.insert(car1);
+    car1.mark = 'mercedes';
+    oldRev = car1.rev;
+
+    cars
+      .update(car1)
+      .flush()
+      .then(function () {
+        done = true;
+      })
+      .otherwise(function (err) {
+        console.log(err);
+      });
+
+    waitsFor(function () {
+      return done;
+    }, 'update', 100);
+
+    runs(function () {
+      var docPath = paths.documentPath(rootDir, car1.id),
+        revPath = paths.revisionPath(rootDir, car1.id, oldRev);
+
+      expect(fs.existsSync(docPath)).toBe(true);
+      expect(fs.existsSync(revPath)).toBe(true);
+      expect(JSON.parse(fs.readFileSync(docPath)).mark).toBe('mercedes');
+    });
+  });
+
+  it('can remove', function () {
+    var done = false;
+
+    cars.insert(car1).flush().then(function () {
+      cars.remove(car1).flush().then(function () {
+        done = true;
+      });
     });
 
-    afterEach(function () {
-        ffs.rmdirRecursiveSync(__dirname + '/var');
+    waitsFor(function () {
+      return done;
+    }, 'remove car', 100);
+
+    runs(function () {
+      expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(false);
     });
+  });
 
-    it('can insert', function () {
-        var done = false;
+  it('can remove in sequence', function () {
+    var done = false;
 
-        cars.insert(car1).flush()
-            .then(function () {
-                done = true;
-            }).otherwise(function (err) {
-                console.log(err.stack);
-            });
+    cars.insert(car1)
+      .remove(car1)
+      .flush()
+      .then(function () {
+        done = true;
+      });
 
-        waitsFor(function () {
-            return done;
-        }, 'insert', 100);
+    waitsFor(function () {
+      return done;
+    }, 'remove in sequence', 100);
 
-        runs(function () {
-            expect(
-                fs.existsSync(paths.documentPath(rootDir, car1.id))
-            ).toBe(true);
-        });
+    runs(function () {
+      expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(false);
     });
+  });
 
-    it('can update', function () {
-        var done = false,
-            oldRev;
+  it('save inserts when no record found', function () {
+    var done = false;
 
-        cars.insert(car1);
-        car1.mark = 'mercedes';
-        oldRev = car1.rev;
+    cars
+      .save(car1)
+      .flush()
+      .then(function () {
+        done = true;
+      });
 
-        cars
-            .update(car1)
-            .flush()
-            .then(function () {
-                done = true;
-            })
-            .otherwise(function (err) {
-                console.log(err);
-            });
+    waitsFor(function () {
+      return done;
+    }, 'save insert', 100);
 
-        waitsFor(function () {
-            return done;
-        }, 'update', 100);
-
-        runs(function () {
-            var docPath = paths.documentPath(rootDir, car1.id),
-                revPath = paths.revisionPath(rootDir, car1.id, oldRev);
-
-            expect(fs.existsSync(docPath)).toBe(true);
-            expect(fs.existsSync(revPath)).toBe(true);
-            expect(JSON.parse(fs.readFileSync(docPath)).mark).toBe('mercedes');
-        });
+    runs(function () {
+      expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(true);
     });
+  });
 
-    it('can remove', function () {
-        var done = false;
+  it('save updates when record found', function () {
+    var done = false;
 
-        cars.insert(car1).flush().then(function () {
-            cars.remove(car1).flush().then(function () {
-                done = true;
-            });
-        });
+    cars.insert(car1)
+      .flush()
+      .then(function () {
+        car1.model = 'foo';
+        return cars
+          .save(car1)
+          .flush();
+      })
+      .then(function () {
+        done = true;
+      });
 
-        waitsFor(function () {
-            return done;
-        }, 'remove car', 100);
+    waitsFor(function () {
+      return done;
+    }, 'save insert', 100);
 
-        runs(function () {
-            expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(false);
-        });
+    runs(function () {
+      expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(true);
     });
-
-    it('can remove in sequence', function () {
-        var done = false;
-
-        cars.insert(car1)
-            .remove(car1)
-            .flush()
-            .then(function () {
-                done = true;
-            });
-
-        waitsFor(function () {
-            return done;
-        }, 'remove in sequence', 100);
-
-        runs(function () {
-            expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(false);
-        });
-    });
-
-    it('save inserts when no record found', function () {
-        var done = false;
-
-        cars
-            .save(car1)
-            .flush()
-            .then(function () {
-                done = true;
-            });
-
-        waitsFor(function () {
-            return done;
-        }, 'save insert', 100);
-
-        runs(function () {
-            expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(true);
-        });
-    });
-
-    it('save updates when record found', function () {
-        var done = false;
-
-        cars.insert(car1)
-            .flush()
-            .then(function () {
-                car1.model = 'foo';
-                return cars
-                    .save(car1)
-                    .flush();
-            })
-            .then(function () {
-                done = true;
-            });
-
-        waitsFor(function () {
-            return done;
-        }, 'save insert', 100);
-
-        runs(function () {
-            expect(fs.existsSync(paths.documentPath(rootDir, car1.id))).toBe(true);
-        });
-    });
+  });
 
 });
---
+++ spec/revisions.spec.js
@@ -0,0 +1,88 @@
+/*jshint node:true*/
+/*global describe, it, beforeEach, afterEach, expect, jasmine, waitsFor, runs*/
+
+'use strict';
+
+var fdb = require('../index.js');
+var fs = require('fs');
+var paths = require('../lib/paths.js');
+var ffs = require('final-fs');
+
+describe('actions', function () {
+  var cars1;
+  var cars2;
+  var car;
+  var carsDir1 = __dirname + '/var/cars1';
+  var carsDir2 = __dirname + '/var/cars2';
+
+  beforeEach(function () {
+    cars1 = new fdb.Collection({dirName: carsDir1, storeRevisions: true});
+    cars2 = new fdb.Collection({dirName: carsDir2, storeRevisions: false});
+    car = {
+      mark: 'Fiat',
+      model: '126p'
+    };
+  });
+
+  afterEach(function () {
+    ffs.rmdirRecursiveSync(__dirname + '/var');
+  });
+
+  it('can store revisions', function () {
+    var revisions = null;
+
+    cars1
+      .insert(car)
+      .flush()
+      .then(function () {
+        return cars1.update(car).flush()
+      })
+      .then(function () {
+        return ffs.readdirRecursive(carsDir1 + '/revisions', true);
+      })
+      .then(function (files) {
+        revisions = files;
+      })
+      .otherwise(function (err) {
+          console.log('err', [err, err.stack]);
+      });
+
+    waitsFor(function () {
+      return revisions;
+    }, 100, 'car to be stored');
+
+    runs(function () {
+      expect(revisions.length).toBe(1);
+    });
+
+  });
+
+  it('can turn off revisions', function () {
+    var revisionsDirExists = null;
+
+    cars2
+      .insert(car)
+      .flush()
+      .then(function () {
+        return cars2.update(car).flush()
+      })
+      .then(function () {
+        return ffs.exists(carsDir2 + '/revisions');
+      })
+      .then(function (exists) {
+        revisionsDirExists = exists;
+      })
+      .otherwise(function (err) {
+        console.log('err', [err, err.stack]);
+      });
+
+    waitsFor(function () {
+      return revisionsDirExists !== null;
+    }, 100, 'car to be stored');
+
+    runs(function () {
+      expect(revisionsDirExists === false).toBeTruthy();
+    });
+  });
+
+});
